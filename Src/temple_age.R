512067c06e84c20e1d5be473bc7f1a6b142eabc3
wccarleton <ccarleton@protonmail.com>
2022-12-13 12:37:58 +0100

    working on viz

--- Src/temple_age.R
+++ Src/temple_age.R
@@ -648,20 +648,12 @@ m
 
 library(shiny)
 
-r_colors <- rgb(t(col2rgb(colors()) / 255))
-names(r_colors) <- colors()
-
 ui <- fluidPage(
   leafletOutput("AngkorTemples"),
   p(),
-  actionButton("recalc", "New points")
 )
 
 server <- function(input, output, session) {
-
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
 
   output$AngkorTemples <- renderLeaflet({
     m <- leaflet()
